33cddc06608a98d17597083d27811b397bd46daa
Titouan_LeBreton <titoulebreton12@gmail.com>
2025-11-05 11:13:18 +0100

    gpu and cpu comparaison for vector add

--- vector_add/main.cu
+++ vector_add/main.cu
@@ -1,6 +1,9 @@
 #include<cuda_runtime.h>
 #include <iostream>
+#include <chrono>
+#include <cstdlib>
 using namespace std;
+using namespace std::chrono;
 
 // Kernel definition
 __global__ void VecAdd(float* A, float* B, float* C)
@@ -34,6 +37,12 @@ void add_vector_device(const float* h_A, const float *h_B, float *h_C, int N){
 
 }
 
+void add_vector_host(const float* h_A, const float* h_B, float* h_C, int N){
+    for (int i=0; i<N; i++){
+        h_C[i] = h_A[i] + h_B[i];
+    }
+}
+
 void print_array(float* x, int N){
     for (int i=0; i<N; i++){
         cout << x[i] <<" ";
@@ -41,15 +50,15 @@ void print_array(float* x, int N){
     cout << endl;
 }
 
-int main()
+int main(int argc, char** argv)
 {   
-    int N = 100 + 1;
-    cout << "Implementation 1" << endl;
+    int N = (argc > 1) ? static_cast<int>(atof(argv[1])) : 1e3;
+
     // declare the host and device pointers
     float *h_A = new float[N];
     float *h_B = new float[N];
     float *h_C = new float[N];
-    float *d_A, *d_B, *d_C;
+    float *h_C2 = new float[N];
 
     // fill in the device variables:
     for (int i=0; i<N; i++){
@@ -57,28 +66,21 @@ int main()
         h_B[i] = 2 * i;
     }
 
-    cudaMalloc(&d_A, N * sizeof(float));
-    cudaMalloc(&d_B, N * sizeof(float));
-    cudaMalloc(&d_C, N * sizeof(float));
-
-    cudaMemcpy(d_A, h_A, N * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, h_B, N * sizeof(float), cudaMemcpyHostToDevice);
-
-
-    VecAdd<<<1, N>>>(d_A, d_B, d_C);
-
-    cudaMemcpy(h_C, d_C, N * sizeof(float), cudaMemcpyDeviceToHost);
-
-    print_array(h_C, N);
-
-    cudaFree(d_A);
-    cudaFree(d_B);
-    cudaFree(d_C);
-
-    cout << "implementation 2" << endl;
-
+    auto d_start = high_resolution_clock::now();
     add_vector_device(h_A, h_B, h_C, N);
-    print_array(h_C, N);
+    auto d_end = high_resolution_clock::now();
+    auto d_duration = duration_cast<milliseconds>(d_end - d_start);
+    //print_array(h_C, N);
+
+    auto h_start = high_resolution_clock::now();
+    add_vector_host(h_A, h_B, h_C2, N);
+    auto h_end = high_resolution_clock::now();
+    auto h_duration = duration_cast<milliseconds>(h_end - h_start);
+    cout <<endl;
+    //print_array(h_C2, N);
+    cout << endl;
+    cout << "Time for gpu operations: " << d_duration.count() << " milli seconds" << endl;
+    cout << "Time for cpu operations: " << h_duration.count() << " milli seconds" << endl;
 
     delete[] h_A;
     delete[] h_B;
