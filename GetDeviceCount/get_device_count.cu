b1da512d10795c17d46cdc2ac400ca59b4623f58
T-prog123 <titoulebreton12@gmail.com>
2025-12-23 15:52:37 +0000

    "polished GetDeviceCount"

--- GetDeviceCount/get_device_count.cu
+++ GetDeviceCount/get_device_count.cu
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <cuda_runtime.h>
+
 using namespace std;
+
+
+#define CUDA_CHECK(call)                                                     \
+  do {                                                                       \
+    cudaError_t err = (call);                                                \
+    if (err != cudaSuccess) {                                                \
+      fprintf(stderr, "CUDA error %s:%d: %s (%s)\n",                          \
+              __FILE__, __LINE__, cudaGetErrorString(err),                   \
+              cudaGetErrorName(err));                                        \
+      abort();                                                               \
+    }                                                                        \
+  } while (0)
 
 int main(){
     int count;
-    cudaGetDeviceCount(&count);
+    CUDA_CHECK(cudaGetDeviceCount(&count));
     cout << "The number of devices we have are " << count << endl;
     return 0;
 }
