4dec44ca026f955bfe84e94e95549a86b3f5a383
T-prog123 <titoulebreton12@gmail.com>
2025-12-23 12:18:05 +0000

    "counting devices in CUDA works!"

--- GetDeviceCount/get_device_count.cu
+++ GetDeviceCount/get_device_count.cu
@@ -1,7 +1,9 @@
 #include <iostream>
-int main{
+using namespace std;
+
+int main(){
     int count;
-    cudaGetDeviceCount(count);
-    cout << "The number of devices we have are" << count << endl;
-    return 0
+    cudaGetDeviceCount(&count);
+    cout << "The number of devices we have are " << count << endl;
+    return 0;
 }
