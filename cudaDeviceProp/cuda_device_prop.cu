d764c3dd0ed546f6d81224906481f6c99c31719f
Titouan_LeBreton <titoulebreton12@gmail.com>
2025-12-25 17:44:17 +0100

    " added grid and block dimensions to cuda_device_prop"

--- cudaDeviceProp/cuda_device_prop.cu
+++ cudaDeviceProp/cuda_device_prop.cu
@@ -9,11 +9,24 @@ int main(){
     cout << "Device name: " << device_1_prop.name << endl;
     cout << "Max global memory (VRAM): " << device_1_prop.totalGlobalMem << endl;
     cout << "Max shared memory (per block): " << device_1_prop.sharedMemPerBlock << endl;
-    cout << "Max threads per block: " << device_1_prop.maxThreadsPerBlock << endl;
-    cout << "Wrap size (for threads) " << device_1_prop.wrapSize << endl;
+
     cout << endl;
     
+    cout << "Max grid dimensions (x, y, z): "
+     << device_1_prop.maxGridSize[0] << ", "
+     << device_1_prop.maxGridSize[1] << ", "
+     << device_1_prop.maxGridSize[2] << endl;
+     
+    cout << "Max block dimensions (x, y, z): "
+     << device_1_prop.maxThreadsDim[0] << ", "
+     << device_1_prop.maxThreadsDim[1] << ", "
+     << device_1_prop.maxThreadsDim[2] << endl;
+
+    cout << "Max threads per block: " << device_1_prop.maxThreadsPerBlock << endl;
+    cout << "Wrap size (for threads) " << device_1_prop.warpSize << endl;
+    cout << endl; 
     cout << "Device Major: " << device_1_prop.major << endl;
     cout << "Device Minor: " << device_1_prop.minor << endl;
+    cout << "Number of streaming Multi-processors: " << device_1_prop.multiProcessorCount;
     return 0;
 }
